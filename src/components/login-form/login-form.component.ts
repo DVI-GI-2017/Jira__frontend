3419932c503f4bf8c3db7099b19c1792fd5d622d
vladpereskokov <vladpereskokov@mail.ru>
2017-08-22 22:58:26 +0300

    add validate login form

--- src/components/login-form/login-form.component.ts
+++ src/components/login-form/login-form.component.ts
@@ -14,17 +14,29 @@ import './login-form.component.scss';
                           <p class="error__text center-align">• Ошибка</p>
                           <div class="row">
                             <div class="input-field col s12">
-                              <input id="email" type="email" class="validate form__input-color form__label-color">
+                              <input 
+                              id="email" 
+                              type="email" 
+                              class="form__input-color form__label-color"
+                              v-model='loginEmailInput'
+                              @keyup='checkEmail'>
                               <label for="email" class="form__label-color">Email</label>
                             </div>
                           </div>
                           <div class="row">
                             <div class="input-field col s12">
-                              <input id="password" type="password" class="validate form__input-color form__label-color">
+                              <input 
+                              id="password" 
+                              type="password" 
+                              class="form__input-color form__label-color"
+                              v-model='loginPasswordInput'>
                               <label for="password">Password</label>
                             </div>
                           </div>
-                          <a class="waves-effect waves-light btn-large button__login button__login__color">
+                          <a 
+                          class="waves-effect waves-light btn-large button__login button__login__color"
+                          @:click='eventForm'
+                          :disabled='getIsValid'>
                             Войти
                           </a>
                         </form>
@@ -34,4 +46,31 @@ import './login-form.component.scss';
             </div>`
 })
 export class LoginFormComponent extends Vue {
+  public loginEmailInput: string = '';
+  public loginPasswordInput: string = '';
+  private emailRegExp: RegExp = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+  private isValid: boolean = false;
+
+  public checkEmail(): boolean {
+    return this.loginEmailInput.length > 5 && this.emailRegExp.test(this.loginEmailInput);
+  }
+
+  public checkPassword() {
+    return this.loginPasswordInput.length > 8;
+  }
+
+  public eventForm() {
+    this.$emit('login-data', this.formData);
+  }
+
+  public get getIsValid() {
+    return !this.checkEmail() || !this.checkPassword();
+  }
+
+  public get formData() {
+    return {
+      email: this.loginEmailInput,
+      password: this.loginPasswordInput
+    };
+  }
 }
--- src/pages/MainPage/MainPage.page.ts
+++ src/pages/MainPage/MainPage.page.ts
@@ -1,5 +1,6 @@
 import * as Vue from 'vue';
 import Component from 'vue-class-component';
+import * as fetch from 'isomorphic-fetch';
 
 import {HeaderComponent} from '../../components/header/header.component';
 import {LoginFormComponent} from '../../components/login-form/login-form.component';
@@ -8,7 +9,7 @@ import {LoginFormComponent} from '../../components/login-form/login-form.compone
   template: `
     <div>
       <header-component></header-component>
-      <login-form-component></login-form-component>
+      <login-form-component @login-data='getTestInput'></login-form-component>
       <router-view></router-view>
     </div>
   `,
@@ -18,4 +19,10 @@ import {LoginFormComponent} from '../../components/login-form/login-form.compone
   }
 })
 export class MainPage extends Vue {
+  public mounted() {
+  }
+
+  public getTestInput(value: any) {
+    console.log(value)
+  }
 }
