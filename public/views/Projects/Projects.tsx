0f2b2a15b083c35f9df1125e1c6ec97c7e6b1028
vladpereskokov <vladpereskokov@mail.ru>
2017-08-24 00:17:21 +0300

    add card

--- public/views/Projects/Projects.tsx
+++ public/views/Projects/Projects.tsx
@@ -1,5 +1,4 @@
 import * as React from 'react';
-import {Linked} from '../../components/Linked/Linked';
 import {Content} from '../../components/Content/Content';
 
 import './Projects.scss';
@@ -28,16 +27,67 @@ export class Projects extends React.Component<any, any> {
                 <p className="center-align text__decorator projects__text">
                   To do 6
                 </p>
+                <div className="column task__position">
+                  <div className="post-module hover">
+                    <div className="thumbnail">
+                      <div className="date">
+                        <div className="day">27</div>
+                        <div className="month">Mar</div>
+                      </div><img src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/169963/photo-1429043794791-eb8f26f44081.jpeg"/>
+                    </div>
+                    <div className="post-content">
+                      <div className="category">Photos</div>
+                      <h1 className="title">City Lights in New York</h1>
+                      <h2 className="sub_title">The city that never sleeps.</h2>
+                      <p className="description">New York, the largest city in the U.S., is an architectural marvel with plenty of historic monuments, magnificent buildings and countless dazzling skyscrapers.</p>
+                      <div className="post-meta"><span className="timestamp"><i className="fa fa-clock-o"></i> 6 mins ago</span></div>
+                    </div>
+                  </div>
+                </div>
               </div>
               <div className="w3-col w3-container m4 l4">
                 <p className="center-align text__decorator projects__text">
                   In progress 2
                 </p>
+                <div className="column task__position">
+                  <div className="post-module hover">
+                    <div className="thumbnail">
+                      <div className="date">
+                        <div className="day">27</div>
+                        <div className="month">Mar</div>
+                      </div><img src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/169963/photo-1429043794791-eb8f26f44081.jpeg"/>
+                    </div>
+                    <div className="post-content">
+                      <div className="category">Photos</div>
+                      <h1 className="title">City Lights in New York</h1>
+                      <h2 className="sub_title">The city that never sleeps.</h2>
+                      <p className="description">New York, the largest city in the U.S., is an architectural marvel with plenty of historic monuments, magnificent buildings and countless dazzling skyscrapers.</p>
+                      <div className="post-meta"><span className="timestamp"><i className="fa fa-clock-o"></i> 6 mins ago</span></div>
+                    </div>
+                  </div>
+                </div>
               </div>
               <div className="w3-col w3-container m4 l4">
                 <p className="center-align text__decorator projects__text">
                   Done 145
                 </p>
+                <div className="column task__position">
+                  <div className="post-module hover">
+                    <div className="thumbnail">
+                      <div className="date">
+                        <div className="day">27</div>
+                        <div className="month">Mar</div>
+                      </div><img src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/169963/photo-1429043794791-eb8f26f44081.jpeg"/>
+                    </div>
+                    <div className="post-content">
+                      <div className="category">Photos</div>
+                      <h1 className="title">City Lights in New York</h1>
+                      <h2 className="sub_title">The city that never sleeps.</h2>
+                      <p className="description">New York, the largest city in the U.S., is an architectural marvel with plenty of historic monuments, magnificent buildings and countless dazzling skyscrapers.</p>
+                      <div className="post-meta"><span className="timestamp"><i className="fa fa-clock-o"></i> 6 mins ago</span></div>
+                    </div>
+                  </div>
+                </div>
               </div>
             </div>
           </div>
