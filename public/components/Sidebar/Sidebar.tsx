54dcfd7457fc0cbec9a49807cf868d5a4983cbda
vladpereskokov <vladpereskokov@mail.ru>
2017-08-23 21:52:46 +0300

    sidebar check

--- public/components/Sidebar/Sidebar.tsx
+++ public/components/Sidebar/Sidebar.tsx
@@ -8,10 +8,10 @@ export class Sidebar extends React.Component<any, any> {
     return (
       <div className="w3-sidebar w3-light-grey w3-bar-block left__navbar">
         <Linked pathTo="/projects">
-          <p className="w3-bar-item w3-button">Projects</p>
+          <p className="w3-bar-item w3-button">Project1</p>
         </Linked>
         <Linked pathTo="/tasks">
-          <p className="w3-bar-item w3-button">Tasks</p>
+          <p className="w3-bar-item w3-button">Project2</p>
         </Linked>
       </div>
     );
