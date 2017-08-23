5985fd1128d5f46e7006fe1dad3c987b506cf65d
vladpereskokov <vladpereskokov@mail.ru>
2017-08-23 23:56:50 +0300

    add grid

--- public/views/Projects/Projects.tsx
+++ public/views/Projects/Projects.tsx
@@ -23,15 +23,23 @@ export class Projects extends React.Component<any, any> {
         <div className="w3-container projects__top projects__text-tasks">
           <h1 className="center-align projects__text-description">Tasks</h1>
           <div className="projects__top">
-            <p className="center-align text__decorator projects__text">
-              <Linked pathTo="/tasks?filter=current">Текущих задач 6</Linked>
-            </p>
-            <p className="center-align text__decorator projects__text">
-              <Linked pathTo="/tasks?filter=work">В работе 2</Linked>
-            </p>
-            <p className="center-align text__decorator projects__text">
-              <Linked pathTo="/tasks?filter=closed">Завершено 145</Linked>
-            </p>
+            <div className="w3-row">
+              <div className="w3-col w3-container m4 l4">
+                <p className="center-align text__decorator projects__text">
+                  To do 6
+                </p>
+              </div>
+              <div className="w3-col w3-container m4 l4">
+                <p className="center-align text__decorator projects__text">
+                  In progress 2
+                </p>
+              </div>
+              <div className="w3-col w3-container m4 l4">
+                <p className="center-align text__decorator projects__text">
+                  Done 145
+                </p>
+              </div>
+            </div>
           </div>
         </div>
       </Content>
