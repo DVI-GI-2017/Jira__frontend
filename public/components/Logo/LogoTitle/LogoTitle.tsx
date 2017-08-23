7f2d4a587fa5f66e2d5215d1e556311d79802425
vladpereskokov <vladpereskokov@mail.ru>
2017-08-23 14:41:49 +0300

    change button

--- public/components/Logo/LogoTitle/LogoTitle.tsx
+++ public/components/Logo/LogoTitle/LogoTitle.tsx
@@ -3,7 +3,7 @@ import * as React from 'react';
 export class LogoTitle extends React.Component<any, any> {
   render() {
     return (
-      <div>SOUL HUNTING</div>
+      <div></div>
     );
   }
 }
