908ac4bea65696d0496ffb13f534ea5b6be2c069
vladpereskokov <vladpereskokov@mail.ru>
2017-08-24 09:47:59 +0300

    add logout f

--- public/components/Header/HeaderNav/HeaderNav.tsx
+++ public/components/Header/HeaderNav/HeaderNav.tsx
@@ -1,5 +1,6 @@
 import * as React from 'react';
 import {Linked} from '../../Linked/Linked';
+import {browserHistory} from 'react-router';
 
 export class HeaderNav extends React.Component<any, any> {
   render() {
@@ -8,9 +9,17 @@ export class HeaderNav extends React.Component<any, any> {
         <ul className='ul__class'>
           <li className='li__class'><Linked pathTo='/projects'><p>Projects</p></Linked></li>
           <li className='li__class'><Linked pathTo='/tasks'><p>Tasks</p></Linked></li>
-          <li className='li__class'><Linked pathTo='/tasks'><p>Logout</p></Linked></li>
+          <li className='li__class' onClick={this.logout}><p>Logout</p></li>
         </ul>
       </div>
     );
+  }
+
+  private logout() {
+    if (localStorage.getItem('token')) {
+      localStorage.removeItem('token');
+    }
+
+    browserHistory.push('/');
   }
 }
