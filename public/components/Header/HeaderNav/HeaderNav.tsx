6b88863d5116cb3484d9415ba2bc3704648891f0
vladpereskokov <vladpereskokov@mail.ru>
2017-08-24 08:57:04 +0300

    fix header

--- public/components/Header/HeaderNav/HeaderNav.tsx
+++ public/components/Header/HeaderNav/HeaderNav.tsx
@@ -5,10 +5,10 @@ export class HeaderNav extends React.Component<any, any> {
   render() {
     return (
       <div className='header__wrapper__help'>
-        <ul>
-          <li><Linked pathTo="/projects"><p>Projects</p></Linked></li>
-          <li><Linked pathTo="/tasks"><p>Tasks</p></Linked></li>
-          <li><Linked pathTo="/tasks"><p>Logout</p></Linked></li>
+        <ul className='ul__class'>
+          <li className='li__class'><Linked pathTo='/projects'><p>Projects</p></Linked></li>
+          <li className='li__class'><Linked pathTo='/tasks'><p>Tasks</p></Linked></li>
+          <li className='li__class'><Linked pathTo='/tasks'><p>Logout</p></Linked></li>
         </ul>
       </div>
     );
