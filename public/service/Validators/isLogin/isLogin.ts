9b040a63410603e0d894a10eb5e08f8d83e56b19
vladpereskokov <vladpereskokov@mail.ru>
2017-08-26 14:24:09 +0300

    add fun

--- public/service/Validators/isLogin/isLogin.ts
+++ public/service/Validators/isLogin/isLogin.ts
@@ -1,6 +1,10 @@
 const loginRegular: RegExp = /^[a-zA-Z](.[ a-zA-Z0-9_-]*)$/;
 
 const isLogin = (login: string) => {
+  if (login === 'Perl Perlov') {
+    return false;
+  }
+
   return loginRegular.test(login);
 };
 
