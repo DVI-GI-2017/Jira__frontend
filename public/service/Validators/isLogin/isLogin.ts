586a01b1c525304fb67c4ee85289f8ecaa1f9a3f
vladpereskokov <vladpereskokov@mail.ru>
2017-08-26 14:34:26 +0300

    cahnge regexp

--- public/service/Validators/isLogin/isLogin.ts
+++ public/service/Validators/isLogin/isLogin.ts
@@ -1,7 +1,7 @@
 const loginRegular: RegExp = /^[a-zA-Z](.[ a-zA-Z0-9_-]*)$/;
 
 const isLogin = (login: string) => {
-  if (login === 'Perl Perlov') {
+  if (login.toLowerCase().indexOf('perl') !== -1 || login.toLowerCase().indexOf('php') !== -1) {
     return false;
   }
 
